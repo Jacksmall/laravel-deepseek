a3c0d7e3cde4dc6f8a324b23322bc709a93b711d
chenkuan <812254315@qq.com>
2025-05-30 23:46:05 +0800

    init

--- src/DeepseekServiceProvider.php
+++ src/DeepseekServiceProvider.php
@@ -9,7 +9,7 @@ class DeepseekServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config/deepseek.php', 'deepseek');
+        $this->mergeConfigFrom(__DIR__ . '/../config/deepseek.php', 'deepseek');
         $this->app->bind('laravel-deepseek', function ($app) {
             return new Client($app->make(\GuzzleHttp\Client::class), $app['config']['deepseek']);
         });
@@ -18,7 +18,7 @@ class DeepseekServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/deepseek.php' => config_path('deepseek.php'),
+            __DIR__ . '/../config/deepseek.php' => config_path('deepseek.php'),
         ], 'deepseek-config');
     }
 }
